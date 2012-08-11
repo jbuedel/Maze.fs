1e7e8b72a2672cd5a3e79f1c124a5bba65437482
Josh Buedel <jbuedel@gmail.com>
2012-08-11 13:07:39 -0500

    I'm not really sure where this was going.

--- Maze.fs/Program.fs
+++ Maze.fs/Program.fs
@@ -1,2 +1,54 @@
 ﻿// Learn more about F# at http://fsharp.net
+//
+//let Maze = [ [0;0;0;0;0;0;0;0];
+//             [0;0;0;0;0;0;0;0];
+//             [0;0;0;0;0;0;0;0];
+//             [0;0;0;0;0;0;0;0];
+//             [0;0;0;0;0;0;0;0];
+//             [0;0;0;0;0;0;0;0];
+//             [0;0;0;0;0;0;0;0];
+//             [0;0;0;0;0;0;0;0] ]
+let Unk =  ' '
+let Path = '.'
+let Wall = '#'
+type Pos = int*int
+type Move = 
+| Some of Pos*Pos
+| None
 
+let mazeSize = 10
+let Maze = Array2D.init mazeSize mazeSize (fun x y -> null)
+
+let printMaze (maze: char[,]) =
+    printfn "----------"
+    for x in seq {0 .. Array2D.length1 maze - 1 } do
+        for y in seq {0 .. Array2D.length2 maze - 1 } do
+            printf "%c" maze.[x,y]
+        printfn ""
+
+let onBoard (pos:Pos) = 
+    let x,y = pos
+    x >= 0 && x < mazeSize && y >= 0 && y < mazeSize
+
+let canBePath (maze:Maze) (move:Move) =
+    let _,(too) = move
+    let to_x,to_y = too
+    onBoard too &&  maze.[to_x,to_y] = null
+
+let rec next (maze: Move[,]) (move:Move) =
+    let pos,too = move
+    let x,y = too
+    maze.[x, y] <- move
+    printMaze maze
+
+    let up:Move =    too, (x+1, y)
+    let down:Move =  too, (x-1, y)
+    let left:Move =  too, (x, y+1)
+    let right:Move = too, (x, y-1)
+
+    let moves = left :: right :: up :: down :: []
+    List.iter (fun move -> if canBePath maze move then next maze move) moves 
+
+
+let final = next Maze (0,0)
+printMaze Maze
