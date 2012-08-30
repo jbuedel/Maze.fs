d9c8855dc265845bf51d5bd7aa96e9dc6fe28261
Josh Buedel <jbuedel@gmail.com>
2012-08-29 23:26:00 -0500

    Randomized room count when none is given.

--- FsCsMvc41/FsCsMvc41WebApp/HomeController.fs
+++ FsCsMvc41/FsCsMvc41WebApp/HomeController.fs
@@ -15,8 +15,10 @@ type HomeController() =
     inherit Controller()
     member this.Index (seed:Nullable<int>, hallWidth:Nullable<int>, rooms:Nullable<int>) =
         let seed = if seed.HasValue then seed.Value else (int)DateTime.Now.Ticks
+        let rand = (new Random(seed)).Next
+
         let hallWidth = if hallWidth.HasValue then hallWidth.Value else 5
-        let rooms = if rooms.HasValue then rooms.Value else 2
+        let rooms = if rooms.HasValue then rooms.Value else rand(5)
         this.View({Seed = seed; HallWidth = hallWidth; Rooms = rooms}) :> ActionResult
 
 
