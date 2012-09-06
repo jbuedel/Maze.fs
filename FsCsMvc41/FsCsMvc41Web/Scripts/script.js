edab6119d5b7c65994283190c018bfac4f30085e
Josh Buedel <jbuedel@gmail.com>
2012-09-05 20:37:28 -0500

    Added a basic js script file.

---
+++ FsCsMvc41/FsCsMvc41Web/Scripts/script.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/Scripts/jquery-1.6.4-vsdoc.js"/>
+
+$(function () {
+    var circle = $("#circle");
+
+
+    $("svg").click(function () {
+        circle.attr('cx', circle.attr('cx')+3);
+    });
+});
