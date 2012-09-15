209c4754a567a7c7fe466f4e74db8d90bab7fc90
Josh Buedel <jbuedel@gmail.com>
2012-09-15 13:42:56 -0500

    The little green cirlce now navigates the maze properly.

--- FsCsMvc41/FsCsMvc41Web/Scripts/script.js
+++ FsCsMvc41/FsCsMvc41Web/Scripts/script.js
@@ -2,16 +2,61 @@
 /// <reference path="~/Scripts/d3.js"/>
 
 $(function () {
+    // draw the maze
+/*
+    var maze = $("#maze g");
+    for (var i = 0; i < all_walls.length; i++) {
+        var wall = all_walls[i];
+        //var line = '<line x1="' + wall.p1.x + '" y1="' + wall.p1.y + '" x2="' + wall.p2.x + '" y2="' + wall.p2.y + '" stroke-width="4" stroke="#000000" fill="none" />';
+        var line = $("<line>").attr('y1', wall.p1.y).attr('x1', wall.p1.x).attr('x2', wall.p2.x).attr('y2', wall.p2.y).attr('stroke-width', 4).attr('stroke', "#000000").attr('fill', 'none');
+        $(line).appendTo(maze);
+    }
+*/
+});
+
+
+
+function IsOnSegment(xi, yi, xj, yj,
+                         xk, yk) {
+    return (xi <= xk || xj <= xk) && (xk <= xi || xk <= xj) &&
+         (yi <= yk || yj <= yk) && (yk <= yi || yk <= yj);
+}
+
+function ComputeDirection(xi, yi, xj, yj,
+                              xk, yk) {
+    var a = (xk - xi) * (yj - yi);
+    var b = (xj - xi) * (yk - yi);
+    return a < b ? -1 : a > b ? 1 : 0;
+}
+
+// Do line segments (x1, y1)--(x2, y2) and (x3, y3)--(x4, y4) intersect? 
+function DoLineSegmentsIntersect(x1, y1, x2, y2,
+                              x3, y3, x4, y4) {
+    var d1 = ComputeDirection(x3, y3, x4, y4, x1, y1);
+    var d2 = ComputeDirection(x3, y3, x4, y4, x2, y2);
+    var d3 = ComputeDirection(x1, y1, x2, y2, x3, y3);
+    var d4 = ComputeDirection(x1, y1, x2, y2, x4, y4);
+    return (((d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0)) &&
+          ((d3 > 0 && d4 < 0) || (d3 < 0 && d4 > 0))) ||
+         (d1 == 0 && IsOnSegment(x3, y3, x4, y4, x1, y1)) ||
+         (d2 == 0 && IsOnSegment(x3, y3, x4, y4, x2, y2)) ||
+         (d3 == 0 && IsOnSegment(x1, y1, x2, y2, x3, y3)) ||
+         (d4 == 0 && IsOnSegment(x1, y1, x2, y2, x4, y4));
+}
+
+var pathsIntersect = function (path1, path2) {
+    return DoLineSegmentsIntersect(path1.p1.x,path1.p1.y,path1.p2.x,path1.p2.y, 
+                                   path2.p1.x,path2.p1.y,path2.p2.x,path2.p2.y);
+};
+
+$(function () {
+
     var move = function (attrName, change) {
         var attr = $("#circle").attr(attrName);
         attr.baseVal.value = attr.baseVal.value + change;
     };
 
-    var pathsIntersect = function (path1, path2) {
-        return false;
-    };
-
-    var intersectsAny = function(all_walls, path) {
+    var intersectsAny = function (all_walls, path) {
         for (var i = 0; i < all_walls.length; i++) {
             if (pathsIntersect(all_walls[i], path)) return true;
         }
@@ -49,4 +94,18 @@ $(function () {
 
         e.stopPropagation();
     });
+    
+
+
 });
+
+test("intersectionWorks", function () {
+    var line2 = { p1: { x: 3, y: 3 }, p2: { x: 5, y: 5} };
+    var path1 = { p1: { x: 5, y: 3 }, p2: { x: 3, y: 5} };
+
+    equals(pathsIntersect(path1, line2), true, "These lines should form an x.");
+    equals(pathsIntersect(path1, { p1: { x: 0, y: 0 }, p2: { x: 9, y: 0} }), false, "Do not intersect.");
+    equals(pathsIntersect(path1, path1), true, "Same line. Incident. Considered an intersect.");
+});
+
+
