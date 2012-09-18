de0f9f8a9269030f84bb2bec55dd684494514581
Josh Buedel <jbuedel@gmail.com>
2012-09-17 19:35:26 -0500

    Added an action for resulting maze data as json.  Doesn't work though.

--- FsCsMvc41/FsCsMvc41WebApp/HomeController.fs
+++ FsCsMvc41/FsCsMvc41WebApp/HomeController.fs
@@ -24,3 +24,12 @@ type HomeController() =
         this.View({Seed = seed; HallWidth = hallWidth; Rooms = rooms; MazeHeight = 520; MazeWidth = 520}) :> ActionResult
 
 
+    member this.Maze (seed:Nullable<int>, hallWidth:Nullable<int>, rooms:Nullable<int>) = 
+        let seed = if seed.HasValue then seed.Value else (int)DateTime.Now.Ticks
+        let rand = (new Random(seed)).Next
+
+        let hallWidth = if hallWidth.HasValue then hallWidth.Value else 5
+        let rooms = if rooms.HasValue then rooms.Value else rand(5)
+         
+        let maze = Maze.MakeMeAMaze seed hallWidth rooms
+        this.Json(maze) :> ActionResult
