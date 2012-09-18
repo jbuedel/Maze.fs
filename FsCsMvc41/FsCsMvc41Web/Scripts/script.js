e4341f784e51627a90e84122aaebeb0e7643823a
Josh Buedel <jbuedel@gmail.com>
2012-09-17 22:00:00 -0500

    Moved tests to their own file.

--- FsCsMvc41/FsCsMvc41Web/Scripts/script.js
+++ FsCsMvc41/FsCsMvc41Web/Scripts/script.js
@@ -103,15 +103,6 @@ $(function () {
 });
 
 
-/*
-test("intersectionWorks", function () {
-    var line2 = { p1: { x: 3, y: 3 }, p2: { x: 5, y: 5} };
-    var path1 = { p1: { x: 5, y: 3 }, p2: { x: 3, y: 5} };
 
-    equals(pathsIntersect(path1, line2), true, "These lines should form an x.");
-    equals(pathsIntersect(path1, { p1: { x: 0, y: 0 }, p2: { x: 9, y: 0} }), false, "Do not intersect.");
-    equals(pathsIntersect(path1, path1), true, "Same line. Incident. Considered an intersect.");
-});
-*/
 
 
